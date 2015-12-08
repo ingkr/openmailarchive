c3047822ddeefb3da7a2abb49933424c1e00042a
ingkr <reinsdorf08141>
2015-12-08 22:00:14 +0100

    work in progress

--- openmailarchive/urls.py
+++ openmailarchive/urls.py
@@ -17,14 +17,17 @@
 #
 from django.conf.urls import url
 from django.contrib import admin
-
+from django.conf.urls.static import static
 
 from django.conf.urls import url
+from django.conf import settings
 
 from webview import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
-]
+    url(r'^login/$', 'django.contrib.auth.views.login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout'),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
