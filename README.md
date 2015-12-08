46ebc003e2db795939f9fd49d568cdc3092af34b
Ingmar Kroll <ingkr@localhost.localdomain>
2015-12-08 20:50:18 +0100

    add licenses text

--- README.md
+++ README.md
@@ -1,8 +1,38 @@
 # OpenMailArchive
-
+## Überblick
 OpenMailArchive ist eine Open Source Webanwendung, welche zum Sichern und Archivieren von IMAP- Mailpostfächern verwendet werden kann.
 
 Die Software basiert auf python-django und imapbackup.
 
-Aktuell befindet sich die Software in der Entwicklungsphase und sollte nicht produktiv eingesetzt werden.
+<font color="#aa0000">Aktuell befindet sich die Software in der Entwicklungsphase und sollte nicht produktiv eingesetzt werden.</font>
+
+## Lizenz
+
+<a href ="http://www.gnu.de/documents/gpl.de.html" target="_blank">GNU General Public License Version 3</a>
+
+## Roadmap und Zeitplan
+
+Projektstart: 01.12.2015
+
+Alpha- Release: geplant für Februar 2016
+
+Stable- Release: geplant für Mai 2016
+
+## Kontakt
+
+Kontakt zum Entwickler:
+
+*   Ingmar Kroll <ingmar_kroll@web.de>
+
+## Spenden
+
+Haben wir mit dieser Software Ihr Interesse geweckt? Dann unterstützen Sie doch bitte die Entwicklung mit einer kleinen Spende.
+
+
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="YC482QEKPXMBL">
+<input type="image" src="https://www.paypalobjects.com/de_DE/DE/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="Jetzt einfach, schnell und sicher online bezahlen – mit PayPal.">
+<img alt="" border="0" src="https://www.paypalobjects.com/de_DE/i/scr/pixel.gif" width="1" height="1">
+</form>
 
--- openmailarchive/urls.py
+++ openmailarchive/urls.py
@@ -1,19 +1,20 @@
-"""openmailarchive URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Add an import:  from blog import urls as blog_urls
-    2. Import the include() function: from django.conf.urls import url, include
-    3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
-"""
+#
+#  Copyright 2015 Ingmar Kroll <ingmar_kroll@web.de>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
 from django.conf.urls import url
 from django.contrib import admin
 
